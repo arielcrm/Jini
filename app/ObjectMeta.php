cb810f5547665e9cb0f584216dd608fb428b160d
Ariel Carmely <arielcrm@gmail.com>
2015-07-13 01:44:55 +0300

    ertytuytrhrt

---
+++ app/ObjectMeta.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ObjectMeta extends Model
+{
+    protected $table = 'object_meta';
+
+    /**
+     * Get the object's author.
+     *
+     * @return User
+     */
+    public function object()
+    {
+        return $this->belongsTo('App\Object', 'object_id');
+    }
+
+    public static function getMeta($objectId, $metaKey) {
+        return ObjectMeta::where('object_id', $objectId)
+            ->where('meta_key', $metaKey)->first();
+    }
+
+    public static function getValue($objectId, $metaKey) {
+        if ($objectMeta = ObjectMeta::getMeta($objectId, $metaKey)) {
+            return $objectMeta->meta_value;
+        }
+    }
+
+    public static function setValue($objectId, $metaKey, $metaValue) {
+        if (!$objectMeta = ObjectMeta::getMeta($objectId, $metaKey)) {
+            $objectMeta = new ObjectMeta();
+            $objectMeta->object_id = $objectId;
+        }
+        $objectMeta->meta_key = $metaKey;
+        $objectMeta->meta_value = $metaValue;
+
+        return $objectMeta->save();
+    }
+}
