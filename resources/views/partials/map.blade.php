bca08c23fad0e36a2815aeabf8518481585081cd
Ariel Carmely <arielcrm@gmail.com>
2015-09-02 00:47:29 +0300

    ertytuytrhrtjghfhg

--- resources/views/partials/map.blade.php
+++ resources/views/partials/map.blade.php
@@ -112,8 +112,6 @@
             left: 0 !important;
             background-color: #fff;
             box-shadow: 0 1px 6px rgba(178, 178, 178, 0.6);
-            border: 1px solid rgba(72, 181, 233, 0.6);
-            border-radius: 2px 2px 0 0;
         }
 
     </style>
@@ -202,36 +200,30 @@
             //this.map.setCenter(marker.getPosition());
             if (info == "" || typeof info == 'undefined')
                 return;
-            if (infowindow) {
-                infowindow.close();
-            }
-            infowindow = new google.maps.InfoWindow({
-                content: info,
-                maxWidth: 512,
-                height: 264
-            });
+
+            marker.info = info;
+
+
             google.maps.event.addListener(marker, 'click', function() {
+                if (infowindow) {
+                    infowindow.close();
+                }
+                infowindow = new google.maps.InfoWindow({
+                    content: marker.info,
+                    maxWidth: 512,
+                    height: 264
+                });
+                google.maps.event.addListener(infowindow, 'domready', function() {
+                    var iwOuter = $('.gm-style-iw');
+                    var iwBackground = iwOuter.prev();
+
+                    iwBackground.children(':nth-child(2)').css({'display' : 'none'});
+                    iwBackground.children(':nth-child(4)').css({'display' : 'none'});
+                });
                 infowindow.open(this.map, marker);
             });
 
-            google.maps.event.addListener(infowindow, 'domready', function() {
 
-                // Reference to the DIV which receives the contents of the infowindow using jQuery
-                var iwOuter = $('.gm-style-iw');
-
-                /* The DIV we want to change is above the .gm-style-iw DIV.
-                 * So, we use jQuery and create a iwBackground variable,
-                 * and took advantage of the existing reference to .gm-style-iw for the previous DIV with .prev().
-                 */
-                var iwBackground = iwOuter.prev();
-
-                // Remove the background shadow DIV
-                iwBackground.children(':nth-child(2)').css({'display' : 'none'});
-
-                // Remove the white background DIV
-                iwBackground.children(':nth-child(4)').css({'display' : 'none'});
-
-            });
 
 //            google.maps.event.addListener(marker, 'mouseover', function() {
 //                infowindow.open(map, this);
